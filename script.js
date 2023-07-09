aaa63ae68e6ab831b434048df1ede4ae9e2d717c
kauansales <kauansales@gmail.com>
2023-07-09 12:06:53 -0300

    publicação site

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('h1', { duration: 2000 });
+
+sr.reveal('p', { duration: 2000 });
+
+sr.reveal('button', { duration: 2000 });
+
+sr.reveal('img', { duration: 2000 });
